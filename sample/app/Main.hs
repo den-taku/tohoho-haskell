7866a0d6860073e8c19c3a5a1130fe28eb0b8959
DenTaku <dentaku1412@gmail.com>
2021-02-03 18:42:54 +0900

    Learn tuple

--- sample/app/Main.hs
+++ sample/app/Main.hs
@@ -39,11 +39,23 @@
 --     print '\x3042'
 --     print str
 
-a = map (*2) (reverse (dropWhile (<2) (takeWhile (<5) (1:2:3:[] ++ [3, 4, 5]))))
-b = [x * x | x <- [1..5]]
-c = [x * x | x <- [1..5], x /= 3]
-main = do {
-    print a;
-    print b;
-    print c;
-}
+-- a = map (*2) (reverse (dropWhile (<2) (takeWhile (<5) (1:2:3:[] ++ [3, 4, 5]))))
+-- b = [x * x | x <- [1..5]]
+-- c = [x * x | x <- [1..5], x /= 3]
+-- main = do {
+--     print a;
+--     print b;
+--     print c;
+-- }
+
+a = (1, 'a', "ABC")
+func = return ()
+-- b = fst (1, 'a', "ABC")
+-- c = snd (1, 'a', "ABC")
+(_, _, x) = (1, 'a', "ABC")
+main = do 
+    print a
+    func
+    print x
+    -- print b
+    -- print c
