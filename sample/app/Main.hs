2735d1a7ff78cb6a0ff74487db808c651f8d621d
DenTaku <dentaku1412@gmail.com>
2021-02-03 18:21:18 +0900

    Learn list

--- sample/app/Main.hs
+++ sample/app/Main.hs
@@ -14,12 +14,36 @@
 --     putStrLn "Blue"
 -- }
 
-main = do
-    putStrLn "Red"
-    putStrLn "Blue"
-    putStrLn "Green"
-    putChar 'a'
-    putStr "ABC"
-    putStrLn "ABC"
-    x <- getLine
-    putStrLn x
+-- main = do
+--     putStrLn "Red"
+--     putStrLn "Blue"
+--     putStrLn "Green"
+--     putChar 'a'
+--     putStr "ABC"
+--     putStrLn "ABC"
+--     x <- getLine
+--     putStrLn x
+
+-- x = 123
+
+-- main = print x
+
+-- x :: Int
+-- y :: Int
+-- x = 123
+-- y = 234
+-- str = "Hello \
+--     \world!"
+-- main = do
+--     print $  x + y
+--     print '\x3042'
+--     print str
+
+a = map (*2) (reverse (dropWhile (<2) (takeWhile (<5) (1:2:3:[] ++ [3, 4, 5]))))
+b = [x * x | x <- [1..5]]
+c = [x * x | x <- [1..5], x /= 3]
+main = do {
+    print a;
+    print b;
+    print c;
+}
